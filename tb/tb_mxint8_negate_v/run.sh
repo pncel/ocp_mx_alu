4d02d3cd5b6ca8aa5247068fb7dac35d4ade9152
Xinyu <XinyuGao00@outlook.com>
2024-08-28 16:16:57 -0400

    bash script

---
+++ tb/tb_mxint8_negate_v/run.sh
@@ -0,0 +1,16 @@
+#!/user/bin/bash
+
+$output_vvp="simv"
+$design_dir="../../src/v"
+$tb_dir="."
+$high_tb_dir=".."
+iverilog -g2012 -o $output_vvp \
+    -I $design_dir \
+    "$design_dir/mxint8_negate.v" \
+    "$high_tb_dir/transection.sv" \
+    "$tb_dir/mx_int8_negate_mon.sv" \
+    "$tb_dir/mx_int8_negate_drv.sv" \
+    "$tb_dir/tb_mxint8_negate.sv"
+
+vvp -n $output_vvp > test.log
+
