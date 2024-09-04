f850b4ad08c7b11875e4a53cd35022e1e2261970
Duy Vuong <dvuong365@gmail.com>
2024-09-04 13:24:53 -0700

    added clamping when converting float32 to mxint8

--- src/v/mxint8_broadcast.v
+++ src/v/mxint8_broadcast.v
@@ -13,11 +13,12 @@ module mxint8_broadcast (
     input  wire [`FLOAT32_WIDTH-1:0]         i_float32;
     output reg  [`SCALE_WIDTH-1:0]           o_scale;
     output reg  [`MXINT8_ELEMENT_WIDTH-1:0]  o_mxint8_elements [BLOCK_SIZE-1:0];
-    output reg                               o_overflow;
 
     reg [`FLOAT32_MANTISSA_WIDTH-1:0] rounded_mantissa;
     reg [`FLOAT32_MANTISSA_WIDTH+1:0] extended_mantissa;
-    reg rounded_mantissa_overflow;
+    reg [`FLOAT32_MANTISSA_WIDTH+1:0] clamped_rounded_mantissa;
+    reg [`SCALE_WIDTH-1:0]            temp_scale;
+    reg                               rounded_mantissa_overflow;
 
     // extract mantissa from input float32
     // apply round to nearest even
@@ -61,16 +62,38 @@ module mxint8_broadcast (
         end
     end
 
-    // adjust with the sign because mantissa is unsigned in float32 but signed in mxint8
-    assign extended_mantissa = i_float32[`FLOAT32_SIGN_BIT] ? ~{2'b1, rounded_mantissa} + 1 : {2'b1, rounded_mantissa};
-
     // shared scale is exact same as the biased exponent of float32
     // pass right through
     // if all 1's then NaN or Inf which matches with MX standard for NaN
-    assign o_scale = rounded_mantissa_overflow ? i_float32[`FLOAT32_EXPONENT_BITS] + 8'd1 : i_float32[`FLOAT32_EXPONENT_BITS];
 
-    // if scale overflow due to rounding from the beginning
-    assign o_overflow = (o_scale == 8'b1111_1111) || (i_float32[`FLOAT32_EXPONENT_MSB] ^ o_scale[`SCALE_WIDTH-1]);
+    // if scale overflow due to rounding from the beginning, then clamp value to highest possible MXINT8 value
+    // there is no output overflow flag because of clamping
+    always @(*) begin
+        if (i_float32[`FLOAT32_EXPONENT_BITS] == 8'b1111_1111) begin
+            o_scale = i_float32[`FLOAT32_EXPONENT_BITS];
+            clamped_rounded_mantissa = rounded_mantissa; // don't really care about mantissa here because it is all NaN anyway
+        end
+        else begin
+            if (rounded_mantissa_overflow) begin
+                if ((i_float32[`FLOAT32_EXPONENT_BITS] + 8'd1) == 8'b1111_1111) begin
+                    // o_scale = i_float32[`FLOAT32_EXPONENT_BITS];
+                    o_scale = 8'b1111_1110;
+                    clamped_rounded_mantissa = 6'b11_1111;
+                end
+                else begin
+                    o_scale = i_float32[`FLOAT32_EXPONENT_BITS] + 8'd1;
+                    clamped_rounded_mantissa = rounded_mantissa;
+                end
+            end
+            else begin
+                o_scale = i_float32[`FLOAT32_EXPONENT_BITS];
+                clamped_rounded_mantissa = rounded_mantissa;
+            end
+        end
+    end
+
+    // adjust with the sign because mantissa is unsigned in float32 but signed in mxint8
+    assign extended_mantissa = i_float32[`FLOAT32_SIGN_BIT] ? ~{2'b1, clamped_rounded_mantissa} + 1 : {2'b1, clamped_rounded_mantissa};
 
     // convert the mantissa to the mxint8
     // effectively only take 6 MSB of original mantissa because clamping and avoid needs to round
